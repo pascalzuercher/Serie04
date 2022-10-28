97b2d462f25f4a33085f55839a82734607df3beb
LeandroLuethi <92726813+LeandroLuethi@users.noreply.github.com>
2022-10-28 23:59:47 +0200

    Finished Implementation 1

--- src/Coin.java
+++ src/Coin.java
@@ -4,29 +4,29 @@ public class Coin {
     private boolean side;
 
     public Coin(){
-        this.side=flip();
+        flip();
     }
 
     public boolean getSide() {
         return side;
     }
 
-    public void setSide(boolean side) {
-        this.side = side;
-    }
-
-    public boolean flip(){
+    public void flip(){
         Random rand = new Random();
-        double sidenum = rand.nextInt(0,2);
-        return sidenum == 1;
+        if(rand.nextInt(0, 2)==1)
+            this.side = true;
+        else
+            this.side = false;
     }
 
     @Override
-    public boolean equals(Coin coinobj) {
-        if (this.side == coinobj.getSide())
+    public boolean equals(Object coinobj) {
+        if(this == coinobj)
             return true;
-        else
+        else if(coinobj==null || getClass() != coinobj.getClass())
             return false;
+        Coin coin = (Coin) coinobj;
+        return getSide() == coin.getSide();
     }
 
     @Override
