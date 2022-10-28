9a6412ef3537a064f462b3b70d480776f1bdc57d
LeandroLuethi <92726813+LeandroLuethi@users.noreply.github.com>
2022-10-28 15:30:56 +0200

    Start Imlementation 1

--- src/Coin.java
+++ src/Coin.java
@@ -1,3 +1,12 @@
+import java.util.Random;
+import java.lang.Math;
 public class Coin {
+    boolean side;
+    public Coin(){
+        this.side=flip();
+    }
 
+    public boolean flip(){
+
+    }
 }
