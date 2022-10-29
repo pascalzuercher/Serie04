ccdeb1d468e7f7c09a13bd7f86315d06cf28cb0a
pascalzuercher <pa.zuercher@bluewin.ch>
2022-10-29 17:15:54 +0200

    dice.java

---
+++ src/Dice.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class Dice {
+
+	// maximale Punktezahl des W�rfels
+	public final int MAX = 6;
+
+	// aktuelle Punktezahl des W�rfels
+	private int points;
+
+	// Objekt zur Erzeugung von Zufallszahlen
+	private Random randomGenerator;
+
+	/**
+	 * instanziiert einen neuen W�rfel und initialisiert die Variablen
+	 */
+	public Dice() {
+		this.randomGenerator = new Random();
+		this.points = 1;
+	}
+
+	/**
+	 * "wirft" den W�rfel (setzt die Punktezahl auf eine zuf�llige, g�ltige Zahl)
+	 */
+	public int roll() {
+		this.points = this.randomGenerator.nextInt(MAX) + 1;
+		return this.points;
+	}
+
+	/**
+	 * setzt die aktuelle Punktezahl des W�rfels auf den �bergebenen Wert
+	 */
+	public void setPoints(int points) {
+		if (points >= 1 && points <= MAX)
+			this.points = points;
+		else
+			this.printMessage("Unzul�ssige Eingabe. " + "W�rfel wird nicht ver�ndert.");
+	}
+
+	/**
+	 * gibt die aktuelle Punktezahl des W�rfels zur�ck
+	 */
+	public int getPoints() {
+		return this.points;
+	}
+
+	/**
+	 * gibt eine Systemmeldung aus
+	 */
+	private void printMessage(String message) {
+		System.out.println(message);
+	}
+}
